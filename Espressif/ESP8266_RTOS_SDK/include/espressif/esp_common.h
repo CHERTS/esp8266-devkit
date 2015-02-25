8b59240c076c90e89e4247846f998090be8a2ed8
Mikhail Grigorev <sleuthhound@gmail.com>
2015-02-25 16:18:01 +0500

    Added ESP8266_RTOS_SDK
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

---
+++ Espressif/ESP8266_RTOS_SDK/include/espressif/esp_common.h
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (C) 2013 -2014  Espressif System
+ *
+ */
+
+#ifndef __ESP_COMMON_H__
+#define __ESP_COMMON_H__
+
+#include "c_types.h"
+#include "esp_libc.h"
+#include "esp_misc.h"
+#include "esp_softap.h"
+#include "esp_sta.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+#include "esp_wifi.h"
+
+#include "esp8266/esp8266.h"
+
+#include "spi_flash.h"
+
+#include "version.h"
+#endif
