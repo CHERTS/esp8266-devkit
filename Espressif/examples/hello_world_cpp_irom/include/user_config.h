72538da4486a1401eea7d741228142c8e6058e65
Mikhail Grigorev <sleuthhound@gmail.com>
2015-03-20 11:11:51 +0500

    Updated examples hello_world_cpp_irom
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/hello_world_cpp_irom/include/user_config.h
+++ Espressif/examples/hello_world_cpp_irom/include/user_config.h
@@ -1,4 +1,10 @@
 #ifndef __USER_CONFIG_H__
 #define __USER_CONFIG_H__
 
+#ifdef ICACHE_IRAM
+#define ICACHE_IRAM_ATTR __attribute__((section(".text")))
+#else
+#define ICACHE_IRAM_ATTR
+#endif /* ICACHE_IRAM */
+
 #endif
--- Espressif/examples/hello_world_cpp_irom/user/user_main.cpp
+++ Espressif/examples/hello_world_cpp_irom/user/user_main.cpp
@@ -6,9 +6,9 @@
 #include <osapi.h>
 #include <os_type.h>
 #include <gpio.h>
+#include "user_config.h"
 
 #define DELAY 1000 /* milliseconds */
-#define ICACHE_IRAM_ATTR __attribute__((section(".text")))
 
 // =============================================================================================
 // C includes and declarations
