f5074b5ace6fc7cf96ba1cc5e5d9048115294c6b
Mikhail Grigorev <sleuthhound@gmail.com>
2015-03-25 11:54:24 +0500

    Updated example ESP8266_Relay_Board
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/ESP8266_Relay_Board/user/oled.c
+++ Espressif/examples/ESP8266_Relay_Board/user/oled.c
@@ -11,6 +11,8 @@
 #include "driver/i2c.h"
 #include "driver/i2c_oled.h"
 #include "dht22.h"
+#include "ds18b20.h"
+#include "config.h"
 
 static volatile bool OLED;
 
@@ -35,17 +37,33 @@ static void ICACHE_FLASH_ATTR pollOLEDCb(void * arg){
 		OLED_CLS();
 		char displaystr[32];
 		char temp[16];
+		
+		unsigned long epoch = sntp_time+(sntp_tz*3600);
+		epoch=epoch%86400;
+		unsigned int hour=epoch/3600;
+		
+		if(hour > 6 && hour <23) { //Protect the OLED from burning
+ 
+			if(sysCfg.sensor_dht22_enable) {
+				dht_temp_str(temp);
+				os_sprintf(displaystr, "Temp: %sC    ", temp);
+				OLED_Print(2, 0, (unsigned char *)displaystr, 2);
   
-		dht_temp_str(temp);
-		os_sprintf(displaystr, "Temp: %sC    ", temp);
-		OLED_Print(2, 0, (unsigned char *)displaystr, 2);
-  
-		dht_humi_str(temp);
-		os_sprintf(displaystr, "Humi: %s%%    ", temp);
-		OLED_Print(2, 2, (unsigned char *)displaystr, 2);
-  
-		os_sprintf(displaystr,"%s",epoch_to_str(sntp_time+(sntp_tz*3600)));  
-		OLED_Print(0, 6, (unsigned char *)displaystr, 1);
+				dht_humi_str(temp);
+				os_sprintf(displaystr, "Humi: %s%%    ", temp);
+				OLED_Print(2, 2, (unsigned char *)displaystr, 2);
+			}
+		
+			if(sysCfg.sensor_ds18b20_enable) {
+				ds_str(temp,0);
+				os_sprintf(displaystr, "Temp: %sC    ", temp);
+				OLED_Print(2, 0, (unsigned char *)displaystr, 2);		
+			}
+
+		}
+			
+		//os_sprintf(displaystr,"%s",epoch_to_str(sntp_time+(sntp_tz*3600)));  
+		//OLED_Print(0, 6, (unsigned char *)displaystr, 1);
 	}
 }
 
