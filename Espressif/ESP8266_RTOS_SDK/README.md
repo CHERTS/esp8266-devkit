ed8b8fd25134dc05378ab3fda709cb5c98c9c539
Mikhail Grigorev <sleuthhound@gmail.com>
2015-06-20 00:13:09 +0500

    Updated ESP8266_RTOS_SDK
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

---
+++ Espressif/ESP8266_RTOS_SDK/README.md
@@ -0,0 +1,25 @@
+esp_iot_rtos_sdk
+================
+
+ESP8266 new SDK based on FreeRTOS
+
+   
+Note
+
+APIs of "esp_iot_rtos_sdk" are same as "esp_iot_sdk"
+
+More details in "Wiki" !
+
+   
+Compile
+
+using command： ./gen_misc.sh
+   
+
+Download
+
+eagle.app.v6.flash.bin, downloads to flash 0x00000
+
+eagle.app.v6.irom0text.bin, downloads to flash 0x40000
+
+blank.bin, downloads to flash 0x7E000
--- Espressif/ESP8266_RTOS_SDK/include/espressif/esp_system.h
+++ Espressif/ESP8266_RTOS_SDK/include/espressif/esp_system.h
@@ -9,10 +9,12 @@
 #include "c_types.h"
 
 enum rst_reason {
-	DEFAULT_RST	  = 0,
-	WDT_RST	      = 1,
-	EXCEPTION_RST = 2,
-	SOFT_RST      = 3
+	DEFAULT_RST_FLAG	= 0,
+	WDT_RST_FLAG,
+	EXCEPTION_RST_FLAG,
+	SOFT_WDT_RST_FLAG,
+	SOFT_RESTART_FLAG,
+	DEEP_SLEEP_AWAKE_FLAG
 };
 
 struct rst_info{
@@ -32,11 +34,14 @@ const char* system_get_sdk_version(void);
 
 void system_restore(void);
 void system_restart(void);
+
 void system_deep_sleep(uint32 time_in_us);
+bool system_deep_sleep_set_option(uint8 option);
 
 uint32 system_get_time(void);
 
 void system_print_meminfo(void);
+
 uint32 system_get_free_heap_size(void);
 uint32 system_get_chip_id(void);
 
@@ -47,5 +52,24 @@ bool system_rtc_mem_read(uint8 src, void *dst, uint16 n);
 bool system_rtc_mem_write(uint8 dst, const void *src, uint16 n);
 
 void system_uart_swap(void);
+void system_uart_de_swap(void);
+
+uint16 system_adc_read(void);
+uint16 system_get_vdd33(void);
+
+enum flash_size_map {
+    FLASH_SIZE_4M_MAP_256_256 = 0,
+    FLASH_SIZE_2M,
+    FLASH_SIZE_8M_MAP_512_512,
+    FLASH_SIZE_16M_MAP_512_512,
+    FLASH_SIZE_32M_MAP_512_512,
+    FLASH_SIZE_16M_MAP_1024_1024,
+    FLASH_SIZE_32M_MAP_1024_1024
+};
+
+enum flash_size_map system_get_flash_size_map(void);
+
+bool system_param_save_with_protect(uint16 start_sec, void *param, uint16 len);
+bool system_param_load(uint16 start_sec, uint16 offset, void *param, uint16 len);
 
 #endif
