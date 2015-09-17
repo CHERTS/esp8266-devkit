9e737322d02728c0fa37ccb1ba72c4a934a51ece
Mikhail Grigorev <sleuthhound@gmail.com>
2015-09-17 22:33:42 +0500

    Updated example sysinfo
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/sysinfo/user/user_main.c
+++ Espressif/examples/sysinfo/user/user_main.c
@@ -10,7 +10,19 @@
 
 #define DELAY 3000 /* milliseconds */
 
+const char *FlashSizeMap[] =
+{
+		"512 KB (256 KB + 256 KB)",	// 0x00
+		"256 KB",			// 0x01
+		"1024 KB (512 KB + 512 KB)", 	// 0x02
+		"2048 KB (512 KB + 512 KB)"	// 0x03
+		"4096 KB (512 KB + 512 KB)"	// 0x04
+		"2048 KB (1024 KB + 1024 KB)"	// 0x05
+		"4096 KB (1024 KB + 1024 KB)"	// 0x06
+};
+
 extern int ets_uart_printf(const char *fmt, ...);
+int (*console_printf)(const char *fmt, ...) = ets_uart_printf;
 
 LOCAL os_timer_t info_timer;
 
@@ -20,19 +32,14 @@ void user_rf_pre_init(void)
 
 LOCAL void ICACHE_FLASH_ATTR info_cb(void *arg)
 {
-	char temp[80];
-	ets_uart_printf("System info:\r\n");
-	os_sprintf(temp, "SDK version: %s\r\n", system_get_sdk_version());
-	ets_uart_printf(temp);
-	os_sprintf(temp, "Time = %ld\r\n", system_get_time());
-	ets_uart_printf(temp);
-	os_sprintf(temp, "Chip id = 0x%x\r\n", system_get_chip_id());
-	ets_uart_printf(temp);
-	os_sprintf(temp, "CPU freq = %d MHz\r\n", system_get_cpu_freq());
-	ets_uart_printf(temp);
-	os_sprintf(temp, "Free heap size = %d\r\n", system_get_free_heap_size());
-	ets_uart_printf(temp);
-	ets_uart_printf("==========================================\r\n");
+	console_printf("==== System info: ====\r\n");
+	console_printf("SDK version:%s rom %d\r\n", system_get_sdk_version(), system_upgrade_userbin_check());
+	console_printf("Time = %ld\r\n", system_get_time());
+	console_printf("Chip id = 0x%x\r\n", system_get_chip_id());
+	console_printf("CPU freq = %d MHz\r\n", system_get_cpu_freq());
+	console_printf("Flash size map = %s\r\n", FlashSizeMap[system_get_flash_size_map()]);
+	console_printf("Free heap size = %d\r\n", system_get_free_heap_size());
+	console_printf("==== End System info ====\r\n");
 }
 
 void user_init(void)
