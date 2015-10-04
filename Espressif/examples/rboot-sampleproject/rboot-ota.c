a5118dae3cd983b821cd42954367ba52156ee00d
Mikhail Grigorev <sleuthhound@gmail.com>
2015-10-05 00:53:44 +0500

    Fixed rboot-sampleproject
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/rboot-sampleproject/rboot-ota.c
+++ Espressif/examples/rboot-sampleproject/rboot-ota.c
@@ -285,6 +285,7 @@ bool ICACHE_FLASH_ATTR rboot_ota_start(ota_callback callback) {
 	
 	// set update flag
 	system_upgrade_flag_set(UPGRADE_FLAG_START);
+	
 	// set up connection
 	upgrade->conn->type = ESPCONN_TCP;
 	upgrade->conn->state = ESPCONN_NONE;
