08bd2e306d9587cd0f47e654856f7cea5932dc8f
Mikhail Grigorev <sleuthhound@gmail.com>
2016-02-08 16:49:38 +0500

    Updated install-mingw-package.bat
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/install-mingw-package.bat
+++ Espressif/install-mingw-package.bat
@@ -1,6 +1,26 @@
 @echo off
-
-title Installing MinGW packages...
+title Installing additional packages for MinGW
+cls
+echo.
+echo Installing additional packages for MinGW
+echo.
+at > nul
+if not %ERRORLEVEL% EQU 0 (
+      echo Error: To install packages need admin rights.
+      echo.
+      echo Run the script as an administrator.
+      echo.
+      echo To run as administrator, click on the file install.cmd the right mouse
+      echo button and select 'Run as administrator'
+      echo.
+      echo Press Enter to exit.
+      pause >nul
+      goto :eof
+)
+echo Press Ctrl-C to cancel, Enter to continue
+pause >nul
+cd /d %~dp0
+echo.
 
 if exist "C:\MinGW" (
   C:
@@ -19,7 +39,6 @@ if exist "C:\MinGW" (
     mingw-get install libz
     mingw-get install bzip2
     mingw-get install msys-base
-    mingw-get install msys-coreutils
     mingw-get install msys-coreutils-ext
     mingw-get install msys-gcc-bin
     mingw-get install msys-wget-bin
@@ -27,18 +46,29 @@ if exist "C:\MinGW" (
     mingw-get install msys-bison-bin
     mingw-get install msys-flex-bin
     mingw-get install msys-gawk
-    mingw-get install msys-sed
     mingw-get install msys-autoconf
     mingw-get install msys-automake
     mingw-get install msys-mktemp
-    mingw-get install msys-patch
     mingw-get install msys-libtool
-    echo Installing modules for MinGW completed.
+    echo.
+    echo Installing additional packages for MinGW complete.
+    echo.
+    echo Press Enter to exit the installation wizard.
+    echo.
+    pause >nul
   ) else (
+    echo.
     echo ERROR! mingw-get.exe not found.
+    echo.
+    echo Press Enter to exit.
+    echo.
+    pause >nul
+    goto :eof
   )
 ) else (
+  echo.
   echo ERROR! MinGW not found.
+  echo.
+  echo Press Enter to exit.
+  echo.
 )
-pause
-
