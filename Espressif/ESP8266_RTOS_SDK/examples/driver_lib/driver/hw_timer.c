b47e6078adac02ca3ac80f2e9572186e5b255222
Mikhail Grigorev <sleuthhound@gmail.com>
2016-08-16 13:18:42 +0500

    Updated ESP8266_RTOS_SDK
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/ESP8266_RTOS_SDK/examples/driver_lib/driver/hw_timer.c
+++ Espressif/ESP8266_RTOS_SDK/examples/driver_lib/driver/hw_timer.c
@@ -49,7 +49,7 @@ typedef enum {          // timer interrupt mode
 
 static void (* user_hw_timer_cb)(void) = NULL;
 
-static void hw_timer_isr_cb(void *arg)
+static void hw_timer_isr_cb(void)
 {
     if (user_hw_timer_cb != NULL) {
         (*(user_hw_timer_cb))();
