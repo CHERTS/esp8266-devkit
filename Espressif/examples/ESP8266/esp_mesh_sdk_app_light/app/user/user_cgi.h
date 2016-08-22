61b93e489245ed37a8ea6fdb360e3390b708b9f7
Mikhail Grigorev <sleuthhound@gmail.com>
2016-08-22 09:23:10 +0500

    - Deleted examples:   esp_mesh_sdk_app_controller;   esp_mesh_sdk_app_light;   esp_mesh_sdk_app_switch;
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/ESP8266/esp_mesh_sdk_app_light/app/user/user_cgi.h
+++
@@ -1,2 +0,0 @@
-#include "httpd.h"
-int ICACHE_FLASH_ATTR cgiEspApi(HttpdConnData *connData);
--- Espressif/examples/ESP8266/esp_mesh_sdk_app_switch/app/include/user_cgi.h
+++
@@ -1,9 +0,0 @@
-#ifndef __USER_CGI_H__
-#define __USER_CGI_H__
-
-#include "httpd.h"
-
-int ICACHE_FLASH_ATTR cgiGetConfig(HttpdConnData *connData);
-int ICACHE_FLASH_ATTR cgiSetConfig(HttpdConnData *connData);
-
-#endif
