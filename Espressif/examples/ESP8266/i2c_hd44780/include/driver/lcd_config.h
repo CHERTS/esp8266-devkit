9d705fbd20041f10dab02f7f08bdd8ad89e94310
Mikhail Grigorev <sleuthhound@gmail.com>
2017-05-18 22:47:44 +0500

    Delete example i2c_hd44780;
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/examples/ESP8266/i2c_hd44780/include/driver/lcd_config.h
+++
@@ -1,4 +0,0 @@
-#define LCD_COL 20 
-#define LCD_ROW 4
-#define LCD_DOTSIZE 0
-#define LCD_ADDRESS 0x27
--- Espressif/examples/ESP8266/i2c_hd44780/user/user_network.h
+++
@@ -1,6 +0,0 @@
-#include "espconn.h"
-#include "driver/i2c.h"
-#include "driver/i2c_hd44780.h"
-
-void network_init();
-
